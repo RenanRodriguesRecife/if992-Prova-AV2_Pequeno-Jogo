f4729447b8115e9b5e8d44e1a6ab5fdd7510da60
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-10-07 07:17:31 -0300

    jogo

---
+++ src/Jogo.py
@@ -0,0 +1,18 @@
+class Jogo:
+    def __init__(self):
+        self.isRodando = False
+        self.isPausado = False
+        self.pontos = 0
+        self.nivel = 0
+
+    def laco(self): #game loop
+        pass
+
+    def iniciarNivel(self):
+        pass
+
+    def pausarJogo(self):
+        pass
+
+    def finalizarJogo(self):
+        pass
