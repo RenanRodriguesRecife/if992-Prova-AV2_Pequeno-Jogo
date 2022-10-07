74bed61d3e432a5a085485e6a6b8b83db837e9dd
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-10-06 23:57:18 -0300

    ver-coisa

---
+++ src/Coisa.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+class Coisa(ABC):
+    def __init__(self):
+        self.simbolo = '-'
+        self.visivel = True
+        self.area = -1
+        self.posicao = [[],[]]
+
+    @abstractmethod
+    def reagirMovimento(self):
+        pass
+
+    @abstractmethod
+    def entrarArea(self):
+        pass
+
+    @abstractmethod
+    def sairArea(self):
+        pass
+
+    @abstractmethod
+    def mover(self,direcao):
+        pass
