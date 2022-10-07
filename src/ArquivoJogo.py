5fae095f5c71a4ffe52faa6c590974047d724f9b
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-10-07 10:45:17 -0300

    arq jogo

---
+++ src/ArquivoJogo.py
@@ -0,0 +1,10 @@
+class ArquivoJogo:
+    def __init__(self,nomeArquivo,caminhoArquivo):
+        self.nomeArquivo = nomeArquivo
+        self.caminhoArquivo = caminhoArquivo
+
+    def salvarJogo(self,jogo):
+        pass
+
+    def carregarJogo(self):
+        pass
