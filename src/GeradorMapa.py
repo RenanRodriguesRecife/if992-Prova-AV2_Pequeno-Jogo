a77be5ea6a8c5fd9c43cd462b10b913558257ef8
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-10-07 19:18:09 -0300

    geradorMapa

---
+++ src/GeradorMapa.py
@@ -0,0 +1,12 @@
+class GeradorMapa:
+    def __init__(self,coisa,bomba,parede,inimigo):
+        self.probCoisa = coisa
+        self.probBomba = bomba
+        self.probParede = parede
+        self.probInimigo = inimigo
+
+    def gerar(tamanhoMapa):
+        pass
+
+    def adicionarTesouros(quantidade,mapa):
+        pass
