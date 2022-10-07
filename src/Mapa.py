50dd47066c833259b3c7a16d14631bc2a15358a5
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-10-07 07:13:59 -0300

    mapa

---
+++ src/Mapa.py
@@ -0,0 +1,24 @@
+class Mapa:
+    def __init__(self,jogador):
+        self.Coisas = [[],[]]
+        self.Jogador = jogador
+        self.mapaFinalizado = False
+        self.pontos = 0
+
+    def adicionarCoisa(self,tipo,posicao):
+        pass
+
+    def mudarLugar(self,coisa):
+        pass
+
+    def removerCoisa(self,coisa):
+        pass
+
+    def checarLugar(self,posicao):
+        pass
+
+    def finalizarMapa(self):
+        pass
+
+    def encontrarCoisa(self,coisa):
+        pass
