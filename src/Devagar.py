11d92c576eadd15f8427193dd696e5989706b345
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-10-14 09:15:47 -0300

    entrar sair area

--- src/Devagar.py
+++ src/Devagar.py
@@ -2,3 +2,6 @@ class Devagar(Inimigo):
     def __init__(self):
         self.simbolo = 's'
         self.isParado = False
+
+
+    
--- src/Invisivel.py
+++ src/Invisivel.py
@@ -1,3 +1,9 @@
 class Invisivel(Inimigo):
     def __init__(self):
         self.area = 2
+
+    def entrarArea():
+        self.simbolo = 'o'
+
+    def sairArea():
+        self.simbolo = ' '
