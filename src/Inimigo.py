abadedc2005d4d029197001ad59a7ec14348d490
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-10-14 08:31:06 -0300

    escolherAcao

--- src/Inimigo.py
+++ src/Inimigo.py
@@ -1,8 +1,12 @@
 from abc import ABC, abstractmethod
+import random
 
 class Inimigo(Coisa,ABC):
     def __init__(self):
         self.simbolo = 'i'
         
     def escolherAcao(self):
-        pass
+        return random.choice(["cima","baixo","esquerda","direita"])
+
+
+    
